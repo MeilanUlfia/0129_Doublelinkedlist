0d22e228a23fd0173dfcd8641b6a77d9f0acbb5b
meilan <meilanulfia@gmail.com>
2025-05-15 16:49:42 +0700

    step 8 dengan Insert in between Node

--- DoubleLinkedList2.cpp
+++ DoubleLinkedList2.cpp
@@ -56,6 +56,20 @@ public:
             return;
         }
 
+        // Insert in between Node
+        // Step 8: locate position for insertion
+        Node *current = START;
+        while (current->next != NULL && current->next->noMhs < nim)
+        {
+            current = current->next;
+        }
+
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll numbers not allowed" << endl;
+            return;
+        }
+
        
 
     }
